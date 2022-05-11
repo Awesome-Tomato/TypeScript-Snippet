fe385171264c63def13ad86fe1c0f8da28618c21
MaetDol <rndka9@gmail.com>
2022-05-11 23:05:27 +0900

    refactor, 중복되는 코드 제거 및 가독성 개선

--- 00_counter/maetdol/src/index.ts
+++ 00_counter/maetdol/src/index.ts
@@ -3,23 +3,14 @@ import { $ } from './utils.js';
 
 function init() {
   const count = $('.counter__count');
-  if (!count) throw new Error('Count element not found');
 
   const counter = new Counter(0, (current) => {
     count.innerHTML = current.toString();
   });
 
-  const decreaseButton = $('.buttons--decrease');
-  if (!decreaseButton) throw new Error('Decrease button not found');
-  decreaseButton.addEventListener('click', counter.decrease.bind(counter));
-
-  const increaseButton = $('.buttons--increase');
-  if (!increaseButton) throw new Error('Increase button not found');
-  increaseButton.addEventListener('click', counter.increase.bind(counter));
-
-  const resetButton = $('.buttons--reset');
-  if (!resetButton) throw new Error('Reset button not found');
-  resetButton.addEventListener('click', counter.reset.bind(counter));
+  $('.buttons--decrease').addEventListener('click', () => counter.decrease());
+  $('.buttons--increase').addEventListener('click', () => counter.increase());
+  $('.buttons--reset').addEventListener('click', () => counter.reset());
 }
 
 init();
--- 00_counter/maetdol/src/utils.ts
+++ 00_counter/maetdol/src/utils.ts
@@ -1,3 +1,5 @@
 export function $(selector: string) {
-  return document.querySelector(selector);
+  const element = document.querySelector(selector);
+  if (!element) throw new Error(`Element ${selector} not found`);
+  return element;
 }
