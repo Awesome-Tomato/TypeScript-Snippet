0fbd71c3401ece21316fa8f23696eaaf26bda45f
FEhyoeun <khyoeun92@gmail.com>
2022-05-14 20:40:15 +0900

    1) 스타일 적용 2) 일단 돌아가게만... 업데이트........

--- 00_counter/FEhyoeun/src/counter.ts
+++ 00_counter/FEhyoeun/src/counter.ts
@@ -1,5 +1,27 @@
 
-let count: number = 0
+let count: number = 0;
 
-let countElement = document.getElementsByClassName('count')
-console.log(countElement)
+let countElement = document.getElementById('count');
+let decreaseButton = document.getElementById('button-decrease');
+let resetButton = document.getElementById('button-reset');
+let increaseButton = document.getElementById('button-increase');
+const countElementUpdate = () => countElement!.innerText = String(count);
+
+countElementUpdate();
+
+decreaseButton!.addEventListener("click", () => {
+    count >0 ? count-- : count = 0
+    countElementUpdate();
+})
+
+resetButton!.addEventListener("click", () => {
+    count = 0;
+    countElementUpdate();
+})
+
+increaseButton!.addEventListener("click", () => {
+    count++;
+    countElementUpdate();
+})
+
+
