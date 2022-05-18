f103d22c54d2de33e959aab0bdfb82d4a06fb35e
kimhyoeun <khyoeun92@gmail.com>
2022-05-18 22:13:45 +0900

    리뷰 반영

--- 00_counter/FEhyoeun/src/counter.ts
+++ 00_counter/FEhyoeun/src/counter.ts
@@ -1,14 +1,17 @@
 let count: number = 0;
 
 const countElement = document.getElementById('count') as HTMLElement;
-const countElementUpdate = ():string => countElement.innerText = String(count);
+const countElementUpdate = () => {
+    if(countElement === null) return
+    countElement.innerText = String(count)
+};
 
 countElementUpdate();
 const buttonWrapper = document.getElementById("button-wrapper") as HTMLElement;
 buttonWrapper.addEventListener("click", (e) => {
     let target = e.target as HTMLElement;
     if(target.id === 'button-decrease') {
-        count > 0 ? count-- : count = 0
+        count = count > 0 ? count - 1 : 0
         countElementUpdate();
     } else if(target.id === 'button-reset') {
         count = 0;
