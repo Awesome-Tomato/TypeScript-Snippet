fb0bd9b6d86e023d0afc748eb06b75842663fc3c
MaetDol <rndka9@gmail.com>
2022-05-28 02:21:49 +0900

    feat, 타입 단언 함수 구현

--- 01_todo/maetdol/src/utils.ts
+++ 01_todo/maetdol/src/utils.ts
@@ -6,7 +6,33 @@ export function $(selector: string) {
 }
 
 export function toInputElement(element: Element): HTMLInputElement {
-  if ('value' in element) return element as HTMLInputElement;
+  if (element instanceof HTMLInputElement) return element as HTMLInputElement;
 
-  throw Error(`Provided element doesn't have key 'value'`);
+  throw Error(`Provided element is not an input element`);
+}
+
+export function toTemplateElement(element: Element): HTMLTemplateElement {
+  if (element instanceof HTMLTemplateElement)
+    return element as HTMLTemplateElement;
+
+  throw Error(`Provided element is not an template element`);
+}
+
+export function getHTMLElementIn(
+  element: Element | DocumentFragment,
+  selector: string
+): HTMLElement {
+  const childElement = element.querySelector(selector);
+  if (!childElement) throw Error(`${selector} element not found`);
+  return to<HTMLElement>(HTMLElement, childElement);
+}
+
+export function toHTMLElement(node: Node): HTMLElement {
+  return to<HTMLElement>(HTMLElement, node);
+}
+
+export function to<T>(cls: any, object: Object): T {
+  if (object instanceof <any>cls) return <T>object;
+
+  throw new Error(`Object is not a type of ${cls}`);
 }
