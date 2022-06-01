51ef3802e22b0bcddb8b19e283751664d0d7fa52
SuKyoung Shin <sukyoung.dev@gmail.com>
2022-06-01 22:50:24 +0900

    feat: ToDo 추가 및 전체 아이템삭제기능 구현

--- 01_todo/suKyoung/src/index.ts
+++ 01_todo/suKyoung/src/index.ts
@@ -4,14 +4,16 @@ type HTMLElements = {
   buttonEdit: HTMLInputElement | null;
   buttonDelete: HTMLInputElement | null;
   buttonDeleteAll: HTMLInputElement | null;
+  listResult: HTMLUListElement | null;
 }
 
 function HTMLElements(): HTMLElements {
   const form = document.querySelector('form');
   const inputSearch = document.querySelector('#searchInput');
-  const buttonEdit = document.querySelector('#edit');
-  const buttonDelete = document.querySelector('#delete');
+  const buttonEdit = document.querySelector('.edit');
+  const buttonDelete = document.querySelector('.delete');
   const buttonDeleteAll = document.querySelector('#deleteAll');
+  const listResult = document.querySelector('ul');
 
   return {
     form: form as HTMLFormElement,
@@ -19,17 +21,53 @@ function HTMLElements(): HTMLElements {
     buttonEdit: buttonEdit as HTMLInputElement,
     buttonDelete: buttonDelete as HTMLInputElement,
     buttonDeleteAll: buttonDeleteAll as HTMLInputElement,
+    listResult: listResult as HTMLUListElement,
   }
 }
 
-const {form} = HTMLElements();
-form?.addEventListener('submit', function(e) {
+init();
+
+function init() {
+  const {form, buttonDeleteAll} = HTMLElements();
+
+  form?.addEventListener('submit', submitForm);
+  buttonDeleteAll?.addEventListener('click', deleteAll);
+}
+
+function submitForm(e: any) {
   e.preventDefault();
-  console.log( getInputValue() );
-})
+  const {inputSearch, listResult} = HTMLElements();
+  const newGrocery = getInputValue();
+  if (newGrocery === undefined) return;
+
+  const newHTMLLiElement = document.createElement('li');
+  const newHTMLSpanElement = document.createElement('span');
+  const newHTMLEditButtonElement = document.createElement('button');
+  const newHTMLDeleteButtonElement = document.createElement('button');
+
+  newHTMLEditButtonElement.setAttribute('type', 'button');
+  newHTMLDeleteButtonElement.setAttribute('type', 'button');
+  newHTMLEditButtonElement.setAttribute('class', 'edit');
+  newHTMLDeleteButtonElement.setAttribute('class', 'edit');
+  newHTMLEditButtonElement.innerText = 'edit';
+  newHTMLDeleteButtonElement.innerText = 'delete';
+
+  newHTMLSpanElement.innerText = newGrocery;
+  newHTMLLiElement.append(newHTMLSpanElement);
+  newHTMLLiElement.append(newHTMLEditButtonElement);
+  newHTMLLiElement.append(newHTMLDeleteButtonElement);
+  
+  if (inputSearch !== null) inputSearch.value = '';
+  return listResult?.append(newHTMLLiElement);
+}
 
 function getInputValue() {
   const { inputSearch } = HTMLElements();
   return inputSearch?.value;
 }
 
+function deleteAll() {
+  const {listResult} = HTMLElements();
+  if (listResult === null) return;
+  [...listResult.children].forEach(lists => lists.remove()); // HTMLCollection
+}
