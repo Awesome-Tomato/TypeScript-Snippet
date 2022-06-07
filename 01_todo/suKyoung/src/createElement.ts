d3ff8a8d51c2b288b85c9050efdd0d410d2a3d56
SuKyoung Shin <sukyoung.dev@gmail.com>
2022-06-07 22:13:42 +0900

    refactor: 코드리뷰 일부반영

---
+++ 01_todo/suKyoung/src/createElement.ts
@@ -0,0 +1,27 @@
+import { deleteSingleToDoList, editSingleToDoList } from "./crud";
+
+// FIXME: 반복되는 코드를 함수로 변경
+export function createHTMLLiElement(text: string): HTMLLIElement {
+  const newHTMLLiElement = document.createElement('li');
+  const newHTMLSpanElement = document.createElement('span');
+  const newHTMLEditButtonElement = document.createElement('button');
+  const newHTMLDeleteButtonElement = document.createElement('button');
+
+  newHTMLEditButtonElement.setAttribute('type', 'button');
+  newHTMLDeleteButtonElement.setAttribute('type', 'button');
+  newHTMLEditButtonElement.setAttribute('class', 'edit');
+  newHTMLDeleteButtonElement.setAttribute('class', 'delete');
+  newHTMLEditButtonElement.innerText = 'edit';
+  newHTMLDeleteButtonElement.innerText = 'delete';
+  newHTMLEditButtonElement.addEventListener('click', editSingleToDoList);
+  newHTMLDeleteButtonElement.addEventListener('click', deleteSingleToDoList);
+
+  newHTMLSpanElement.setAttribute('class', 'capitalize');
+  newHTMLSpanElement.innerText = text;
+
+  newHTMLLiElement.append(newHTMLSpanElement);
+  newHTMLLiElement.append(newHTMLEditButtonElement);
+  newHTMLLiElement.append(newHTMLDeleteButtonElement);
+
+  return newHTMLLiElement;
+}
---
+++ 01_todo/suKyoung/src/types.ts
@@ -0,0 +1,9 @@
+export type HTMLElements = {
+  form: HTMLFormElement;
+  inputSearch: HTMLInputElement;
+  buttonSubmit: HTMLInputElement;
+  buttonEdit: HTMLInputElement;
+  buttonDeleteAll: HTMLInputElement;
+  listResult: HTMLUListElement;
+  messageAlert: HTMLParagraphElement;
+}
